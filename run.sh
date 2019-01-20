3e8b75dc4615ec3bcae5fdc6750cc45c6af1660d
Martin Michalko <matemichal@gmail.com>
2019-01-20 13:15:18 +0100

    project domain changed to icinga and new variable for cluster mariadb node ips added

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 set -e
 
-cd /data/projects/ansible2-6/pb/ansible-testing_environment/
-time ansible-playbook -i /data/projects/ansible2-6/pb/docker-icinga/inventory \
-create-update-config.yml \
---extra-vars "@/data/projects/ansible2-6/pb/docker-icinga/group_vars/all/test-env-all.yml" \
--e 'ansible_python_interpreter=/usr/bin/python'
+#cd /data/projects/ansible2-6/pb/ansible-testing_environment/
+#time ansible-playbook -i /data/projects/ansible2-6/pb/docker-icinga3/inventory \
+#create-update-config.yml \
+#--extra-vars "@/data/projects/ansible2-6/pb/docker-icinga3/group_vars/all/test-env-all.yml" \
+#-e 'ansible_python_interpreter=/usr/bin/python'
 
-cd /data/projects/ansible2-6/pb/docker-icinga/
+cd /data/projects/ansible2-6/pb/docker-icinga3/
 time ansible-playbook -i inventory packages-req.yml docker.yml docker-registry.yml
 
 time ansible-playbook /data/projects/ansible2-6/pb/ansible-testing_environment/snapshot-create.yml \
---extra-vars "@/data/projects/ansible2-6/pb/docker-icinga/group_vars/all/test-env-all.yml" \
+--extra-vars "@/data/projects/ansible2-6/pb/docker-icinga3/group_vars/all/test-env-all.yml" \
 --extra-vars "snapshot_name=02-after-docker-reg"
 
 time ansible-playbook -i inventory docker-mariadb.yml && \
@@ -23,7 +23,7 @@ time ansible-playbook -i inventory \
 ###if you would like to apply additional changes to docker files and mariadb and icinga2 playbooks
 ### it is enough to revert to snapshot 02-after-docker-reg on all nodes as root user
 #ansible-playbook /data/projects/ansible2-6/pb/ansible-testing_environment/snapshot-revert.yml \
-#--extra-vars "@/data/projects/ansible2-6/pb/docker-icinga/group_vars/all/test-env-all.yml" \
+#--extra-vars "@/data/projects/ansible2-6/pb/docker-icinga3/group_vars/all/test-env-all.yml" \
 #--extra-vars "snapshot_name=02-after-docker-reg"
 #
 ####and after that rerun the playbooks
